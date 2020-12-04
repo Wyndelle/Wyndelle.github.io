31fc6d1c139cd7595fb5bb44297c3f09ac1618ed
Wyndelle <lifeofateenagealien@gmail.com>
2020-12-04 00:18:18 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -27,18 +27,18 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            physikz.addRandomVelocity(circle, canvas, 100, 100);
             view.addChild(circle);
             circles.push(circle);
         }
 
         // TODO 3 / 7 : Call the drawCircle() function
+        var loopsCompleted = 0; 
+        while (loopsCompleted <= 100) {
+            drawCircle();
+            loopsCompleted++
+        }
 
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
-        drawCircle();
            
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -51,21 +51,22 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition(circles[0]);
-            physikz.updatePosition(circles[1]);
-            physikz.updatePosition(circles[2]);
-            physikz.updatePosition(circles[3]);
-            physikz.updatePosition(circles[4]);
-        
+            
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
            
-
+        
             // TODO 8 : Iterate over the array
            
-        }    
+            for (var i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];
 
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle);
+            }
+
+        }
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -80,7 +81,16 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+             if ( circle.x < 0 ) {
+                circle.x = canvas.width;
+            } 
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            } 
+            if ( circle.y < 0 ) {
+                circle.y = canvas.height;
+
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
