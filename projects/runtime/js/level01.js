2afdc681b2e84743a9a21c93953dbaf963babc17
Wyndelle <lifeofateenagealien@gmail.com>
2020-12-15 20:09:52 +0000

    save everything

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -18,7 +18,8 @@ var level01 = function (window) {
             "gameItems": [
                 { "type": "sawblade", "x": 400, "y": groundY },
                 { "type": "sawblade", "x": 600, "y": groundY },
-                { "type": "sawblade", "x": 900, "y": groundY }
+                { "type": "sawblade", "x": 900, "y": groundY },
+
             ]
         };
         window.levelData = levelData;
@@ -26,12 +27,67 @@ var level01 = function (window) {
         game.setDebugMode(true);
 
         // BEGIN EDITING YOUR CODE HERE
+        
 
+
+
+        function createSawBlade(x,y){
         
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+            game.addGameItem(sawBladeHitZone);    
+            var obstacleImage = draw.bitmap('img/sawblade.png');
+            sawBladeHitZone.addChild(obstacleImage);
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+                
+        }
+        createSawBlade(400,200);
+        createSawBlade(500,200);
+            createSawBlade(600,200);
+
+    
         
-        
-        // DO NOT EDIT CODE BELOW HERE
+    for (var i = 0 ; i < levelData.length; i++) {
+        var firstGameItemObject = levelData.gameItems[i];
+        var firstX = firstGameItemObject.x;
+        var firstY = firstGameItemObject.y;
+        createSawBlade(firstX, firstY);
     }
+    function rainClouds(x,y) {
+     
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var rainCloudHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            rainCloudHitZone.x = x;
+            rainCloudHitZone.y = y;
+            game.addGameItem(rainCloudHitZone);    
+            var obstacleImage = draw.bitmap('img/ob.png');
+            rainCloudHitZone.addChild(obstacleImage);
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+    };
+    rainClouds(600,200)
+var enemy =  game.createGameItem('enemy',25);
+var redSquare = draw.rect(50,50,'red');
+redSquare.x = -25;
+redSquare.y = -25;
+enemy.addChild(redSquare);
+enemy.x = 400;
+enemy.y = groundY-50;
+game.addGameItem(enemy);
+enemy.velocityX = -1;
+enemy.onPlayerCollision = function() {
+};
+
+    
+    // code to do something with each element
+}
+        // DO NOT EDIT CODE BELOW HERE
+    
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -27,7 +27,9 @@ var background = function (window) {
         
         // ANIMATION VARIABLES HERE:
         var cloudy;
-     
+
+        //var dews = [];
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -40,31 +42,36 @@ var background = function (window) {
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
-            // var sun = draw.bitmap('img/light.png');
-            // sun.x = 500;
-            // sun.y = -300;
-            // sun.scaleX = .8;
-            // sun.scaleY = .8;
-            // background.addChild(sun);
-            // var cloud = draw.bitmap('img/clouds.png');
-            // cloud.x = 15;
-            // cloud.y = -700;
-            // cloud.scaleX = .8;
-            // cloud.scaleY = .8;
-            // background.addChild(cloud);
+             var sun = draw.bitmap('img/light.png');
+             sun.x = 500;
+             sun.y = -300;
+             sun.scaleX = .8;
+             sun.scaleY = .8;
+             background.addChild(sun);
+             
+             var cloud = draw.bitmap('img/clouds.png');
+             cloud.x = 15;
+             cloud.y = -700;
+             cloud.scaleX = .8;
+             cloud.scaleY = .8;
+             background.addChild(cloud);
 
 
             
             // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
+           // for(var i=0;i<5;++i) {
+   // var dewHeight = 300;
+   // var dew = draw.rect(75,buildingHeight,'LightGray','Black',1);
+   // dew.x = 200*i;
+   // dew.y = groundY-buildingHeight;
+   // background.addChild(dew);
+   // buildings.push(dew)}
             
             // TODO 4: Part 1 - Add a tree
-            cloudy = draw.bitmap('img/cloudss.png');
-            cloudy.x = 400;
-            cloudy.y = 0;
-            background.addChild(cloudy);
-            
-        } // end of render function - DO NOT DELETE
+            cloudy = draw.bitmap('img/cloudyy.png');
+            cloudy.x = -200;
+            cloudy.y = 300;
+            background.addChild(cloudy);      } // end of render function - DO NOT DELETE
         
         
         // Perform background animation
@@ -76,7 +83,11 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 4: Part 2 - Move the tree!
-            
+            cloudy.x = cloudy.x + -2;
+            if(cloudy.x < -1600) {
+    cloudy.x = canvasWidth;
+}
+
             
             // TODO 5: Part 2 - Parallax
             
